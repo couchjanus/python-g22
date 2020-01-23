3a17528916ac2ea8109579771733017148334c42
couch Janus <couchjanus@gmail.com>
2020-01-23 14:25:51 +0200

    added unit 4

--- calc/calc01.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# coding: utf
-
-str_var = "Calculator"
-print("Hello, ", str_var)
-
-print("a: ", 2)
-print("b: ", 2)
-print("a + b = ", 4)
--- sequences/range/factorials_sum.py
+++
@@ -1,9 +0,0 @@
-# Сумма факториалов
-
-n = int(input())
-f = 1
-sum = 0
-for i in range(1, n + 1):
-   f = f * i
-   sum += f
-print (sum)
